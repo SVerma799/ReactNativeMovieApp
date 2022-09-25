f30e1812d86260be83b53d3cb47dc7131bae0fe5
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2022-09-24 22:13:52 -0700

    Changes done in UI for the Movies Tab

--- movie-app/components/List/MovieList.js
+++ movie-app/components/List/MovieList.js
@@ -1,13 +1,31 @@
-import { Text } from "native-base";
+import { Button, HStack, Image, ScrollView, Text, VStack } from "native-base";
 import React from "react";
 
 const MovieList = ({ mvList }) => {
   return (
-    <>
+    <ScrollView>
       {mvList.map((mv) => {
-        return <Text>{mv.title}</Text>;
+        return (
+          <HStack py={4}>
+            <VStack px={4}>
+              <Image
+                source={{
+                  uri: `https://image.tmdb.org/t/p/original/${mv.poster_path}`,
+                }}
+                alt="Alternate Text"
+                size="xl"
+              ></Image>
+            </VStack>
+            <VStack>
+              <Text py={1}>{mv.title}</Text>
+              <Text py={1}>{mv.release_date}</Text>
+              <Text py={1}>{mv.popularity}</Text>
+              <Button w="80%">More Details</Button>
+            </VStack>
+          </HStack>
+        );
       })}
-    </>
+    </ScrollView>
   );
 };
 
